e16a94693f85286e4cc205e133ba2a40dc28ee4a
Mark Wicks <mawicks@gmail.com>
2023-06-23 22:17:41 -0400

    Add data.py and embeddings.py

---
+++ src/queryGPT/data.py
@@ -0,0 +1,168 @@
+from glob import glob
+import io
+import logging
+import os
+import requests
+import tempfile
+import xml.etree.ElementTree as ET
+
+from tqdm import tqdm
+
+from queryGPT.embeddings import compute_search_embeddings
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+IRS_FILE_SEGMENTS = {
+    2022: [
+        "01A",
+        "01B",
+        "01C",
+        "01D",
+        "01E",
+        "01F",
+        "11A",
+        "11B",
+        "11C",
+    ],
+    2023: [
+        "01A",
+        "02A",
+        "03A",
+        "04A",
+        "05A",
+        "05B",
+    ],
+}
+IRS_FILE_TEMPLATE = (
+    "https://apps.irs.gov/pub/epostcard/990/xml/2022/2022_TEOS_XML_{segment}.zip"
+)
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data"))
+FILENAME_GLOB = "2023*_public.xml"
+
+NS = {"irs": "http://www.irs.gov/efile"}
+RETURN_TYPES_TO_SKIP = ("990PF", "990T")
+
+
+def download_and_parse_segment(segment):
+    docs = []
+    url = IRS_FILE_TEMPLATE.format(segment=segment)
+    request_result = requests.get(url)
+
+    with tempfile.NamedTemporaryFile("wb") as zip_file:
+        zip_file.write(request_result.content)
+
+        with tempfile.TemporaryDirectory() as extract_dir:
+            # Python zipfile cannot handle several IRS files so use command line (:)
+            os.system(f"unzip -d {extract_dir} {zip_file.name} > /dev/null ")
+            for filename in glob(os.path.join(extract_dir, "*.xml")):
+                doc = parse(filename)
+                if doc is not None:
+                    docs.append(doc)
+
+    return docs
+
+
+def combine(items):
+    combined = ""
+    for field in items:
+        if field is not None:
+            if combined:
+                combined += "; "
+            combined += field.text
+    return combined
+
+
+def parse(filename):
+    root = ET.parse(filename).getroot()
+
+    # Skip certain types of returns
+    return_type = root.find(".//irs:ReturnTypeCd", NS)
+    if return_type is not None and return_type.text in RETURN_TYPES_TO_SKIP:
+        return
+
+    # Skip non US addresses
+    foreign_address = root.find(".//irs:ForeignAddress", NS)
+    if foreign_address is not None:
+        return
+
+    doc = {}
+    ein = root.find(".//irs:Filer/irs:EIN", NS)
+    doc["EIN"] = ein.text
+
+    tax_year = root.find(".//irs:TaxYr", NS)
+    doc["Tax Year"] = tax_year.text
+
+    tax_period_start = root.find(".//irs:TaxPeriodBeginDt", NS)
+    tax_period_end = root.find(".//irs:TaxPeriodEndDt", NS)
+    doc["Tax Period"] = f"{tax_period_start.text} to {tax_period_end.text}"
+
+    name1 = root.find(".//irs:BusinessName/irs:BusinessNameLine1Txt", NS)
+    name2 = root.find(".//irs:BusinessName/irs:BusinessNameLine2Txt", NS)
+    doc["Name"] = f"{name1.text}{' ' + name2.text if name2 is not None else '' }"
+
+    address = root.find(".//irs:USAddress/irs:AddressLine1Txt", NS).text
+    city = root.find(".//irs:USAddress/irs:CityNm", NS).text
+    state = root.find(".//irs:USAddress/irs:StateAbbreviationCd", NS).text
+    zip = root.find(".//irs:USAddress/irs:ZIPCd", NS).text
+    doc["Address"] = f"{address}, {city}, {state} {zip}"
+
+    mission = root.find(".//irs:MissionDesc", NS)
+    desc = root.find(".//irs:IRS990/irs:Desc", NS)
+    primary_purpose = root.find(".//irs:PrimaryExemptPurposeTxt", NS)
+    doc["Purpose"] = combine((mission, desc, primary_purpose))
+
+    activity_paths = [
+        "irs:ActivityOrMissionDesc",
+        "irs:SummaryOfDirectChrtblActyGrp/irs:Description1Txt",
+        "irs:SummaryOfDirectChrtblActyGrp/irs:Description2Txt",
+        "irs:ProgSrvcAccomActy2Grp/irs:Desc",
+        "irs:ProgSrvcAccomActy3Grp/irs:Desc",
+    ]
+    activities = [root.find(f"..//{path}", NS) for path in activity_paths]
+    combined_activities = combine(activities)
+
+    if combined_activities:
+        doc["Activities"] = combined_activities
+
+    website = root.find(".//irs:WebsiteAddressTxt", NS)
+    doc["Website"] = website.text if website is not None else "None"
+
+    program_accomplishments = root.findall(".//irs:DescriptionProgramSrvcAccomTxt", NS)
+    if program_accomplishments:
+        doc["Accomplishments"] = combine(program_accomplishments)
+
+    program_service_revenue = root.findall(
+        ".//irs:ProgramServiceRevenueGrp/irs:Desc", NS
+    )
+    if program_service_revenue:
+        doc["Revenue"] = combine(program_service_revenue)
+
+    expenses = root.findall(".//irs:OtherExpensesGrp/irs:Desc", NS)
+    if expenses:
+        doc["Expenses"] = combine(expenses)
+
+    return doc
+
+
+def doc_to_string(doc):
+    return "".join([f"{key}: {value}\n" for key, value in doc.items()])
+
+
+if __name__ == "__main__":
+    for year in (2022,):
+        logger.info(f"Processing year: {year}")
+        for segment in tqdm(IRS_FILE_SEGMENTS[year]):
+            logger.info(f"Downloading segment: {segment} ({year})")
+            docs = download_and_parse_segment(segment)
+            logger.info(f"Parsed {len(docs):,d} documents in segment {segment}")
+
+            compute_search_embeddings(
+                docs,
+                doc_to_string,
+                data_dir=DATA_DIR,
+                year=year,
+                segment=segment,
+            )
+            logger.info("done")
---
+++ src/queryGPT/embeddings.py
@@ -0,0 +1,144 @@
+import gc
+import logging
+import os
+import time
+from typing import Callable
+
+import openai
+import pandas as pd
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+VECTOR_SEARCH_FILE_TEMPLATE = (
+    "irs_form_990_embeddings_{year}_{segment}_{chunk_id}.parquet"
+)
+
+# How many embeddings to store in a single file?
+# We limit it because the chunks in these files get generated in memory and
+# read back into memory.
+
+CHUNK_SIZE = 5_000
+RETRY_LIMIT = 5
+
+# How many documents to pass to OpenAPI at one time.
+OPENAI_BATCH_SIZE = 100
+
+
+def embed_chunk(chunk_of_docs, doc_to_string) -> dict[str, object]:
+    """
+    Use OpenAI to Compute the embeddings for the documents in `docs`
+    and return a dictionary containing the documents that were embedded
+    along with the associated embeddings.
+
+    Arguments:
+        chunk_of_docs: list[dict] - Chunk of docs to embed
+        doc_to_string: Callable[[dict], str] - Function that converts a document
+            dict to an embeddable string
+    Returns:
+        A dictionary with two keys: "docs" and "embeddings".
+        The value of "doc" is the original `chunk_of_docs`
+        The value of "embedding" is a list of embeddigns matching the list of `chunk_of_docs`
+    """
+
+    embeddings = []
+    usage = 0
+    for batch_index in tqdm(range(0, len(chunk_of_docs), OPENAI_BATCH_SIZE)):
+        # Convert list of documents to embeddable text
+        batch = list(
+            map(
+                doc_to_string,
+                chunk_of_docs[batch_index : batch_index + OPENAI_BATCH_SIZE],
+            )
+        )
+
+        # OpenAI calls can fail.  Wrap in a retry loop  Try the batch up to
+        # RETRY_LIMIT times
+        result = None
+        for retry_count in range(RETRY_LIMIT):
+            try:
+                result = openai.Embedding.create(
+                    input=batch,
+                    model=EMBEDDING_MODEL,
+                )
+            except Exception as e:
+                logger.warning("OpenAI call failed on following strings:")
+                logger.warning(f"docs: {batch}")
+                print(e)
+            if result is not None:
+                break
+            logger.warning("retrying...")
+            time.sleep(5 * (2**retry_count))
+        else:
+            raise RuntimeError("Too many retries.")
+
+        embeddings.extend([e["embedding"] for e in result["data"]])  # type:ignore
+        usage += result["usage"]["total_tokens"]  # type:ignore
+
+    logger.info(f"Total tokens used: {usage}")
+
+    assert len(embeddings) == len(chunk_of_docs)
+
+    search_data = {
+        "doc": chunk_of_docs,
+        "embedding": embeddings,
+    }
+
+    return search_data
+
+
+def compute_search_embeddings(
+    docs: list[dict],
+    doc_to_string: Callable[[dict], str],
+    data_dir: str,
+    year: int,
+    segment: str,
+    limit: int | None = None,
+):
+    """
+    Compute the embeddings for a list of documents in `docs` and store
+    them as files in `data_dir` use the filename template
+    VECTOR_SEARCH_FILE_TEMPLATE which is parametered by the passed `segment`
+    and an internally generated `chunk_id` if the document list needs
+    to be broken up to process it.
+
+    Arguments:
+        docs: list[dict] - List of document dictionaries
+        doc_to_string: Callable[[dict], str] - Function that converts a document
+            dict to an embeddable string
+        data_dir: str - Path in which to write output files
+        segment: str - Segment ID to use in the file name
+        year: int - Year filed?
+
+    Returns:
+        None
+    """
+
+    # Don't process all the documents if a limit was specified (useful for
+    # computing a partial set of embeddings for testing).
+    docs_to_embed = docs[:limit]
+
+    # Break the documents into chunks because holding a full set of embeddings
+    # in memory can exhaust resources and cause the process to be killed in
+    # contrained environments.
+
+    logger.info(f"Embedding {len(docs_to_embed):,d} documents")
+
+    for chunk_id, chunk_index in enumerate(
+        tqdm(range(0, len(docs_to_embed), CHUNK_SIZE))
+    ):
+        search_data = embed_chunk(
+            docs_to_embed[chunk_index : chunk_index + CHUNK_SIZE], doc_to_string
+        )
+        df = pd.DataFrame(data=search_data)
+
+        vector_search_filename = VECTOR_SEARCH_FILE_TEMPLATE.format(
+            year=year, segment=segment, chunk_id=chunk_id
+        )
+        df.to_parquet(os.path.join(data_dir, vector_search_filename))
+
+        # As mentioned above, instantiating embeddings in memory consumes it.
+        # Help out the garbage collector by telling it when we're completely
+        # finished with a batch
+        gc.collect()
